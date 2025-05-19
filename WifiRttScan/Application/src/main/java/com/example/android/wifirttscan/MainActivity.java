a2593ee604c15563553474ace015b769a249b096
IGNNE <30634883+IGNNE@users.noreply.github.com>
2025-05-19 14:21:34 +0200

    Make compilable with recent android studio, add recording scanned APs to list

--- WifiRttScan/Application/src/main/java/com/example/android/wifirttscan/MainActivity.java
+++ WifiRttScan/Application/src/main/java/com/example/android/wifirttscan/MainActivity.java
@@ -19,27 +19,37 @@ import static com.example.android.wifirttscan.AccessPointRangingResultsActivity.
 
 import android.Manifest.permission;
 import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
-import android.net.wifi.rtt.RangingRequest;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.RecyclerView.LayoutManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.LayoutManager;
+
 import com.example.android.wifirttscan.MyAdapter.ScanResultClickListener;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -84,6 +94,8 @@ public class MainActivity extends AppCompatActivity implements ScanResultClickLi
         mAdapter = new MyAdapter(mAccessPointsSupporting80211mc, this);
         mRecyclerView.setAdapter(mAdapter);
 
+        createNotificationChannel();
+
         mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         mWifiScanReceiver = new WifiScanReceiver();
     }
@@ -93,12 +105,9 @@ public class MainActivity extends AppCompatActivity implements ScanResultClickLi
         Log.d(TAG, "onResume()");
         super.onResume();
 
-        mLocationPermissionApproved =
-                ActivityCompat.checkSelfPermission(this, permission.ACCESS_FINE_LOCATION)
-                        == PackageManager.PERMISSION_GRANTED;
+        mLocationPermissionApproved = ActivityCompat.checkSelfPermission(this, permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
 
-        registerReceiver(
-                mWifiScanReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        registerReceiver(mWifiScanReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
     }
 
     @Override
@@ -124,6 +133,20 @@ public class MainActivity extends AppCompatActivity implements ScanResultClickLi
         startActivity(intent);
     }
 
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        CharSequence name = "main";
+        String description = "main_desc";
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel("42", name, importance);
+        channel.setDescription(description);
+        // Register the channel with the system; you can't change the importance
+        // or other notification behaviors after this.
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+    }
+
     public void onClickFindDistancesToAccessPoints(View view) {
         if (mLocationPermissionApproved) {
             logToUi(getString(R.string.retrieving_access_points));
@@ -136,21 +159,66 @@ public class MainActivity extends AppCompatActivity implements ScanResultClickLi
         }
     }
 
+
     private class WifiScanReceiver extends BroadcastReceiver {
 
-        private List<ScanResult> find80211mcSupportedAccessPoints(
-                @NonNull List<ScanResult> originalList) {
+        private void writeToFile(String data) {
+            /*try {
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(getApplicationContext().openFileOutput("aps.txt", Context.MODE_APPEND));
+                outputStreamWriter.write(data);
+                outputStreamWriter.close();
+            } catch (IOException e) {
+                Log.e("Exception", "File write failed: " + e.toString());
+            }*/
+            File path = getApplicationContext().getExternalFilesDir(null);
+            File file = new File(path, "APs.csv");
+            FileOutputStream stream = null;
+            try {
+                stream = new FileOutputStream(file, true);
+                stream.write(data.getBytes());
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } finally {
+                if (stream != null) {
+                    try {
+                        stream.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+
+        private List<ScanResult> find80211mcSupportedAccessPoints(@NonNull List<ScanResult> originalList) {
             List<ScanResult> newList = new ArrayList<>();
+
 
             for (ScanResult scanResult : originalList) {
 
+                // log all to file
+                writeToFile(scanResult.SSID + ";" + scanResult.BSSID + ";" +
+                                scanResult.is80211mcResponder() + ";" +
+                                scanResult.capabilities + ";" + scanResult.frequency + ";" +
+                                scanResult.channelWidth + "\n");
+
                 if (scanResult.is80211mcResponder()) {
                     newList.add(scanResult);
+
+                    //Define Notification Manager
+                    NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    //Define sound URI
+                    Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                    NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext(), "42").setSmallIcon(R.drawable.ic_round_network_wifi_white_24px).setContentTitle(scanResult.SSID);
+                    //.setSound(soundUri); //This sets the sound to play
+                    //Display notification
+                    notificationManager.notify(0, mBuilder.build());
                 }
 
-                if (newList.size() >= RangingRequest.getMaxPeers()) {
-                    break;
-                }
+                //if (newList.size() >= RangingRequest.getMaxPeers()) {
+                //    break;
+                //}
             }
             return newList;
         }
@@ -168,11 +236,7 @@ public class MainActivity extends AppCompatActivity implements ScanResultClickLi
 
                     mAdapter.swapData(mAccessPointsSupporting80211mc);
 
-                    logToUi(
-                            scanResults.size()
-                                    + " APs discovered, "
-                                    + mAccessPointsSupporting80211mc.size()
-                                    + " RTT capable.");
+                    logToUi(scanResults.size() + " APs discovered, " + mAccessPointsSupporting80211mc.size() + " RTT capable.");
 
                 } else {
                     // TODO (jewalker): Add Snackbar regarding permissions
